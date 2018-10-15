4f3662fb5775b5247cda4808a0b6a4ad88f5e00f
Vincent Bundage II <vincent.bundage@gmail.com>
2018-10-14 21:31:05 -0600

    Adding exercises from chapter 6

---
+++ chapter_6/exercise_4.rb
@@ -0,0 +1,10 @@
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+1. arr.index(5)
+# return 3
+2. arr.index[5]
+# NoMethodError: undefined method `[]' for #<Enumerator: [15, 7, 18, 5, 12, 8, 5, 1]:index>
+#        from (irb):57
+#        from C:/Ruby24-x64/bin/irb.cmd:19:in `<main>'
+3. arr[5]
+# return 8
---
+++ chapter_6/exercise_6.rb
@@ -0,0 +1,12 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+
+
+TypeError: no implicit conversion of String into Integer
+  from (irb):2:in `[]='
+  from (irb):2
+  from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+# Attempting to reference array index by value
+# Use the index number 3 to modify the value of 'margaret' to 'jody'
+# names[3] = 'jody'
